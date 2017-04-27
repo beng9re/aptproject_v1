22d5834b859995603ce9fa5458dc9cbdd48419e8
DESKTOP-6A2IFIJ\jm <woans0104@gmail.com>
2017-04-27 15:20:31 +0900

    아파트요약보기 수정중

--- aptproject/src/apt/test/NewAptPanel.java
+++ aptproject/src/apt/test/NewAptPanel.java
@@ -14,12 +14,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
+
+import com.sun.javafx.collections.MappingChange.Map;
+import com.sun.javafx.scene.paint.GradientUtils.Parser;
 
 import complex.regist.table.TableController;
 import db.DBManager;
@@ -40,7 +45,18 @@ public class NewAptPanel extends JPanel implements ActionListener {
 	// 호수를 담는 벡터
 	Vector<ComplexDto> complexData = new Vector<ComplexDto>();
 	// 동을 담는 벡터
-	Vector<Unit> unitData = new Vector<Unit>();
+	
+	
+	
+	HashMap<Integer, Integer> map1=new HashMap<Integer,Integer>();
+	
+	HashMap<Integer,HashMap<Integer, Integer>> kingMap=new HashMap<Integer,HashMap<Integer, Integer>>();
+	
+	
+	
+	
+	
+	//Vector<Unit> unitData = new Vector<Unit>();
 	//동 뿌릴떄 컨트롤 하기 위해 담은 벡터
 	Vector<DrawApt> list = new Vector<DrawApt>();
 	//택배 보관함 여부를 담은 벡터
@@ -104,7 +120,7 @@ public class NewAptPanel extends JPanel implements ActionListener {
 
 		getApt();// dto 구해서 벡터에 담기
 		Boxget();//box 정보 구해서 벡터 담기
-
+		
 	}
 
 	// dto 구해서 벡터에 담기
@@ -112,7 +128,7 @@ public class NewAptPanel extends JPanel implements ActionListener {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 
-		String sql = "select * from complex ORDER BY complex.COMPLEX_ID ASC";
+		String sql = "select * from complex ORDER BY complex.complex_name ASC";
 		try {
 			pstmt = con.prepareStatement(sql);
 			rs = pstmt.executeQuery();
@@ -154,35 +170,57 @@ public class NewAptPanel extends JPanel implements ActionListener {
 
 	// 화면에 뿌릴 호수 구하기
 	public void getUnit() {
-
+		kingMap.remove(kingMap);
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 
-		String sql = "select * FROM UNIT u WHERE u.COMPLEX_ID=? ORDER BY unit_name asc";
+		String sql = "select unit_name FROM view_cput WHERE complex_name=? ORDER BY unit_name asc";
 		try {
 
 			pstmt = con.prepareStatement(sql);
-			index = choice.getSelectedIndex();
+			String index = choice.getSelectedItem();
 
-			if (index - 1 >= 0) {
-				ComplexDto dto = complexData.get(index - 1);
+			//if (index - 1 >= 0) {
+				//ComplexDto dto = complexData.get(index - 1);
 
-				pstmt.setInt(1, dto.getComplex_id());
+				pstmt.setString(1, index);
 				rs = pstmt.executeQuery();
-			}
-			unitData.removeAll(unitData);
-
+				
+			//}
+			//unitData.removeAll(unitData); 데이터 비우기
+			
+			int count=1;
+			int x=100*count;
+			int count1=0;
+			
+			
 			while (rs.next()) {
+				
+				
+				int unit=Integer.parseInt(rs.getString("unit_name").split("\\s")[0]);
+				//System.out.println("이거는 유닛"+unit);
+				int y=unit/(x);
+				
+				if(y==2){
+					kingMap.put(count, map1);
+					count=count+1;
+					count1=0;
+					map1.remove(map1);
+				}
+					
+				
+				map1.put(count1++, unit);
+						
+				/*
 				Unit vo = new Unit();
 
-				vo.setUnit_id(rs.getInt("unit_id"));
 				vo.setUnit_name(rs.getString("unit_name"));
-				vo.setComplex_id(rs.getInt("complex_id"));
-
-				unitData.add(vo);
-
+				
+				unitData.put(vo);
+				 */
 			}
-
+			
+			test();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -208,6 +246,17 @@ public class NewAptPanel extends JPanel implements ActionListener {
 
 	}
 
+	public void test(){
+		System.out.println(kingMap.size()+"이거는 킹맵 사이즈");
+		System.out.println(kingMap.get(1).size()+"이거는 겟 1 사이즈");
+		System.out.println(kingMap.get(2).get(1));
+		System.out.println(kingMap.get(2).get(2));
+		System.out.println(kingMap.get(2).get(3));
+		System.out.println(kingMap.get(2).get(4)+"///////");
+		
+		
+	}
+	
 	// 화면에 아파트 뿌리기
 	public void createApt() {
 		p_center.removeAll();
@@ -228,7 +277,7 @@ public class NewAptPanel extends JPanel implements ActionListener {
 	}
 	//아파트 벡터에 담고 뿌리기
 	public void addApt() {
-		for (int i = list.size()- 1; i >= 0; i--) {
+		for (int i =list.size()-1; i >= 0; i--) {
 			if (list.size() != 0) {
 				System.out.println("asdasd");
 				p_center.add(list.get(i));
